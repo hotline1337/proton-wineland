50d0165c4488268bb6a142efd77d78d63967a598
Erhan Bilgili <erhan.bilgili@gmail.com>
2026-09-19 20:23:19 +0200

    build: Preserve generated Speex configure files.
    Exclude generated autotools files from source synchronization for Speex
    and SpeexDSP. This prevents repeated synchronization from deleting the
    configure scripts generated in the build tree.

--- steamrtdeps/build-speex.mk
+++ steamrtdeps/build-speex.mk
@@ -8,6 +8,31 @@ SPEEX_CONFIGURE_ARGS := \
 	--disable-binaries \
 	--disable-valgrind
 
+# Exclude generated autotools files from the source sync so they are not seen
+# as "extras" by the rsync dry-run (which would force a --delete re-sync that
+# wipes the autoreconf-generated configure mid-build). autoreconf regenerates
+# them in the synced tree. Mirrors openfst.
+SPEEX_SOURCE_ARGS = \
+  --exclude aclocal.m4 \
+  --exclude ar-lib \
+  --exclude autom4te.cache \
+  --exclude compile \
+  --exclude config.guess \
+  --exclude config.h.in \
+  --exclude config.sub \
+  --exclude configure \
+  --exclude depcomp \
+  --exclude install-sh \
+  --exclude ltmain.sh \
+  --exclude m4/libtool.m4 \
+  --exclude m4/ltoptions.m4 \
+  --exclude m4/ltsugar.m4 \
+  --exclude m4/ltversion.m4 \
+  --exclude m4/lt~obsolete.m4 \
+  --exclude Makefile.in \
+  --exclude missing \
+  --exclude test-driver \
+
 $(eval $(call rules-source,speex,$(SRCDIR)/steamrtdeps/speex))
 $(eval $(call rules-configure,speex,i386,unix))
 
--- steamrtdeps/build-speexdsp.mk
+++ steamrtdeps/build-speexdsp.mk
@@ -7,6 +7,29 @@ SPEEXDSP_CONFIGURE_ARGS := \
 	--disable-static \
 	--disable-examples
 
+# See build-speex.mk: keep autoreconf-generated files out of the source sync so
+# the dry-run does not force a --delete re-sync that wipes configure mid-build.
+SPEEXDSP_SOURCE_ARGS = \
+  --exclude aclocal.m4 \
+  --exclude ar-lib \
+  --exclude autom4te.cache \
+  --exclude compile \
+  --exclude config.guess \
+  --exclude config.h.in \
+  --exclude config.sub \
+  --exclude configure \
+  --exclude depcomp \
+  --exclude install-sh \
+  --exclude ltmain.sh \
+  --exclude m4/libtool.m4 \
+  --exclude m4/ltoptions.m4 \
+  --exclude m4/ltsugar.m4 \
+  --exclude m4/ltversion.m4 \
+  --exclude m4/lt~obsolete.m4 \
+  --exclude Makefile.in \
+  --exclude missing \
+  --exclude test-driver \
+
 $(eval $(call rules-source,speexdsp,$(SRCDIR)/steamrtdeps/speexdsp))
 $(eval $(call rules-configure,speexdsp,i386,unix))
 
